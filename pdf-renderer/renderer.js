999c5e97778fa49b3be3303e61ce6a5cf5a845d5
Silas Knobel <silas.knobel@katun.ch>
2020-10-21 17:00:52 +0200

    initial commit

---
+++ pdf-renderer/renderer.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const chromium = require('chrome-aws-lambda');
+const fs = require('fs');
+const path = require('path');
+const Mustache = require('mustache');
+
+let response;
+
+module.exports.render = async (event, context) => {
+    const body = JSON.parse(event.body);
+    if (!(body.template === null && body.data === null)) {
+        const template = body.template;
+        const templateData = body.data;
+
+        const renderedTemplateHtml = Mustache.render(template, templateData);
+        const fileName = `pdf.pdf`;
+
+        let browser = null;
+        try {
+            browser = await chromium.puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+                dumpio: false,
+                ignoreHTTPSErrors: true,
+            });
+
+            const page = await browser.newPage();
+            await page.setContent(renderedTemplateHtml, {
+                waitUntil: ['networkidle0','load','domcontentloaded'],
+                timeout: 30000
+            })
+
+            const pdf = await page.pdf({
+                format: 'A4',
+                printBackground: true,
+                margin: {
+                    top: '1cm',
+                    right: '1cm',
+                    bottom: '1cm',
+                    left: '1cm'
+                }
+            });
+
+            response = {
+                headers: {
+                    'Content-Type': 'application/pdf',
+                    // 'content-disposition': `attachment; filename=${fileName}`,
+                    'Content-Length': Buffer.byteLength(pdf, 'utf-8')
+                },
+                statusCode: 200,
+                body: pdf.toString('base64'),
+                isBase64Encoded: true
+            };
+        }
+        catch (error) {
+            response = {
+                statusCode: 500,
+                body: JSON.stringify({
+                    statusCode: 500,
+                    message: error.message
+                })
+            };
+        }
+        finally {
+            if (browser !== null) {
+                await browser.close();
+            }
+        }
+    }
+    else {
+        response = {
+            'statusCode': 400,
+            'body': JSON.stringify({
+                statusCode: 400,
+                message: 'Bad Request'
+            })
+        };
+    }
+
+    return response;
+};
